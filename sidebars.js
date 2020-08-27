bde59d086d01bc9a82b18c4537c81601753465e2
eknir <rinkehendriksen@gmail.com>
2020-08-27 09:48:17 +0200

    add tags documentation

--- sidebars.js
+++ sidebars.js
@@ -7,7 +7,8 @@ module.exports = {
     "Get Started": [
         'tutorial/start-your-node',
         'tutorial/starting-a-network',
-        'tutorial/upload-and-download'
+        'tutorial/upload-and-download',
+        'tutorial/tags'
     ],
     "DApps on Swarm": [
         'api-reference/api-reference'
