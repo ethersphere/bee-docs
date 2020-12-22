a608883bba4a4c4a4406e9f8212983847421bd4f
sig <dan@1up.digital>
2020-12-22 10:13:43 +0000

    fix for subdirectory

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,56 +7,11 @@ import useBaseUrl from '@docusaurus/useBaseUrl';
 import styles from './styles.module.css';
 import {Redirect} from '@docusaurus/router';
 
-const features = [
-  {
-    title: <>Join the Swarm</>,
-    imageUrl: 'img/undraw_Around_the_world_re_n353.svg',
-    description: (
-      <>
-        Welcome to the decentralised revolution. Swarm will make our data our own.
-      </>
-    ),
-  },
-  {
-    title: <>The Decentralised Internet</>,
-    imageUrl: 'img/undraw_Master_plan_re_jvit.svg',
-    description: (
-      <>
-        Store and distribute file and data using a global p2p network of storers and relayers.
-      </>
-    ),
-  },
-  {
-    title: <>Get ready to make BZZ</>,
-    imageUrl: 'img/undraw_Bitcoin_P2P_re_1xqa.svg',
-    description: (
-      <>
-        Start running the future Swarm client now and get ahead of the game. An incentivised network is coming.
-      </>
-    ),
-  },
-];
-
-function Feature({imageUrl, title, description}) {
-  const imgUrl = useBaseUrl(imageUrl);
-  return (
-    <div className={clsx('col col--4', styles.feature)}>
-      {imgUrl && (
-        <div className="text--center">
-          <img className={styles.featureImage} src={imgUrl} alt={title} />
-        </div>
-      )}
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </div>
-  );
-}
-
 function Home() {
   const context = useDocusaurusContext();
   const {siteConfig = {}} = context;
   return (
-    <Redirect to="/docs/" />
+    <Redirect to="docs/" />
   );
 }
 
