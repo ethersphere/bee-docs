1db652205ccc6eb3a7a2588e9557ac08e0108ba5
sig <dan@1up.digital>
2021-05-21 09:04:45 +0100

    tidy up

--- ping.sh
+++
@@ -1,4 +0,0 @@
-for i in {0..69}; do
-wget -e robots=off -r -np --page-requisites --no-check-certificate -T10000 -t1 https://bee-${i}.gateway.ethswarm.org/bzz/docs.swarm.eth/
-wget -e robots=off -r -np --page-requisites --no-check-certificate -T10000 -t1 https://bee-${i}.gateway.ethswarm.org/bzz/docs.swarm.eth/docs/
-done
--- test.sh
+++
@@ -1,4 +0,0 @@
-for i in {0..69}; do
-wget -t1 https://bee-${i}.gateway.ethswarm.org/files/30f8a26ba054d3f2277c99f99bbd7ac36e700983ac1f84eb9a5ccdc80248b6d0
-done
-
