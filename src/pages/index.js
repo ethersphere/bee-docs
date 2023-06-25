da67759aa6efcff9559f5f0390706a4fdac9292a
noahniuwa <noahmaizels@protonmail.com>
2023-06-26 00:45:44 +0700

    push

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,22 +18,25 @@ function Home() {
       </div>
       <div className={styles.container}>
         <a className={styles.sectionButton} href="/docs/learn/introduction">
-          <img className={styles.sectionImageLearn} src="img/learn.svg"></img>
+          <img className={styles.sectionImageLearnDark} src="img/learn-dark.svg"></img>
+          <img className={styles.sectionImageLearnLight} src="img/learn-light.svg"></img>
           <h3 className={styles.buttonTitle}>Learn about Swarm</h3>
           <p className={styles.description}>Get to know more about the Swarm's decentralised data storage and distribution technology.</p>
         </a> 
         <a className={styles.sectionButton} href="/docs/desktop/introduction">
-          <img className={styles.sectionImageDesktop} src="img/desktop.svg"></img>
+          <img className={styles.sectionImageDesktopDark} src="img/desktop-dark.svg"></img>
+          <img className={styles.sectionImageDesktopLight} src="img/desktop-light.svg"></img>
           <h3 className={styles.buttonTitle}>Use Swarm Desktop</h3>
           <p className={styles.description}>Install the Swarm Desktop client to quickly spin up a Bee node and start interacting with the Swarm network.</p>
         </a> 
         <a className={styles.sectionButton} href="/docs/bee/installation/quick-start">
-          <img className={styles.sectionImageOperate} src="img/operate.svg"></img>
-            <h3 className={styles.buttonTitle}>Run a Bee Node</h3>
+          <img className={styles.sectionImageDevelopDark} src="img/bee-dark.svg"></img>
+          <img className={styles.sectionImageDevelopLight} src="img/bee-light.svg"></img>            <h3 className={styles.buttonTitle}>Run a Bee Node</h3>
             <p className={styles.description}>Operate a Bee node to connect with other peers all over the world to become part of Swarm network.</p>
         </a>
         <a className={styles.sectionButton} href="/docs/develop/introduction">
-          <img className={styles.sectionImageOperate} src="img/develop.svg"></img>
+        <img className={styles.sectionImageDevelopDark} src="img/develop-dark.svg"></img>
+          <img className={styles.sectionImageDevelopLight} src="img/develop-light.svg"></img>
           <h3 className={styles.buttonTitle}>Develop on Swarm</h3>
           <p className={styles.description}>Swarm empowers developers to create and host decentralised dapps, NFT meta-data, media files, and much more!</p>
         </a>
