34ead8c0ca67b4f4ce83ef0e05201aeb1f5ab06b
NoahMaizels <noahmaizels@protonmail.com>
2023-11-09 22:08:13 +0700

    added sidebar

--- sidebars.js
+++ sidebars.js
@@ -93,6 +93,7 @@ module.exports = {
         'bee/working-with-bee/upgrading-bee',
         'bee/working-with-bee/uninstalling-bee',
         'bee/working-with-bee/light-nodes',
+        'bee/working-with-bee/troubleshooting',
         'bee/working-with-bee/ultra-light-nodes'
       ],
       collapsed: false
